e633a1a950e09096c5dde7ed2a245da7750ff980
Cedric Harris <charrisq@hotmail.com>
2018-02-21 16:25:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
-  def display_board(board)
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
-      puts "x == board[1][1]"
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
